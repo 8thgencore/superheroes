d5ab779aeeaf7893cb3303dc4ca511a4c6b0310a
30128845+8thgencore@users.noreply.github.com <30128845+8thgencore@users.noreply.github.com>
2021-07-27 18:20:36 +0300

    hmetask_3

--- lib/blocs/main_bloc.dart
+++ lib/blocs/main_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:rxdart/rxdart.dart';
+import 'package:http/http.dart' as http;
 
 class MainBloc {
   static const minSymbols = 3;
@@ -20,6 +21,11 @@ class MainBloc {
 
   Future<List<SuperheroInfo>> search(final String text) async {
     await Future.delayed(Duration(milliseconds: 500));
+    final response = await http.get(Uri.parse("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
+    print(response.statusCode);
+    print(response.reasonPhrase);
+    print(response.headers);
+    print(response.body);
     return SuperheroInfo.mocked
         .where((i) => i.name.toLowerCase().contains(text.toLowerCase()))
         .toList();
